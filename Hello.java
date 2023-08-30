a18d7657a6bc974afea8956b2cb566e00d1d30c4
SimounReyes <Sigreyes313@gmail.com>
2023-08-30 21:44:37 +0800

    modified Hello,java and added MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
   public static void main(String[] args){
     System.out.print("Hello, Software Engineering 1!\nStudent name: Reyes, Simoun Irwin G.");
-    System.out.print("That is why developers are highly-paid!");
+    System.out.print("Complicated world of SoftDev that is why it pays much!");
   }
 }
