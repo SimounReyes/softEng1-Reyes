adaf71f11d994c82a69c970742142d892c07ab33
SimounReyes <Sigreyes313@gmail.come>
2023-08-30 17:01:34 +0800

    added a new file browser-support.txt

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
   public static void main(String[] args){
     System.out.print("Hello, Software Engineering 1!\nStudent name: Reyes, Simoun Irwin G.");
+    System.out.print("That is why developers are highly-paid!");
   }
 }
